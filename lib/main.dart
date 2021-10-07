708477cb7a8bb020cc676b4f0f5c7fdcca568d8a
muhammadsyahlani <muhammadsyahlani10@gmail.com>
2021-10-07 20:10:54 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,47 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Builder(builder: (BuildContext context) {
+          return IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+          );
+        }),
         backgroundColor: Colors.green,
         title: Text(
           'Selamat Datang Syahlani',
           style: TextStyle(
             color: Colors.black,
             fontWeight: FontWeight.bold,
+            fontSize: 20,
           ),
         ),
-        actions: [Icon(Icons.account_circle), Icon(Icons.access_alarm)],
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.search_off_outlined),
+            tooltip: 'Open Search',
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.more_vert_outlined),
+            tooltip: 'Open More',
+            alignment: Alignment.centerRight,
+            onPressed: () {},
+          ),
+        ],
+        actionsIconTheme: IconThemeData(color: Colors.black),
         centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.blue,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        padding: EdgeInsets.all(30),
+      child: Icon(Icons.home,
+      color: Colors.red,
+      size: 100.78,  
       ),
     );
   }
